a2d55f04ce7cb639c7f93fb2b529dfed099b180b
trabelsibahe <baha.trabelsi@gmail.com>
2023-04-29 16:39:03 +0100

    bug fixes

--- controllers/demandeController.js
+++ controllers/demandeController.js
@@ -6,115 +6,116 @@ const Validatedemande = require("../validators/demande")
 
 //add demande
 const ajouterDemande = (req, res) => {
-    const { errors, isValid } = Validatedemande(req.body);
+  const { errors, isValid } = Validatedemande(req.body);
 
-    const demandeObj = {
-        user: req.user._id,
-        type: req.body.type,
-        commentaire: req.body.commentaire,
-        etat: req.body.etat = "Réception",
-    }
-    if (!isValid) {
-        res.status(404).json(errors);
-      } else {
+  const demandeObj = {
+    user: req.user._id,
+    type: req.body.type,
+    commentaire: req.body.commentaire,
+    etat: req.body.etat = "Réception",
+  }
+  if (!isValid) {
+    res.status(404).json(errors);
+  } else {
     const demande = new DemandeModel(demandeObj)
 
     demande.save((errors, createdDemande) => {
-        if (errors) return res.status(400).json({ errors })
-        if (createdDemande) {
-            return res.status(200).json({message: "Votre demande a été envoyer avec succès !" , createdDemande })
-        }
+      if (errors) return res.status(400).json({ errors })
+      if (createdDemande) {
+        return res.status(200).json({ message: "Votre demande a été envoyer avec succès !", createdDemande })
+      }
     })
-}}
-  
+  }
+}
+
 //  Upload Image Controller
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploadsattestation')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-  })
-  
-  const upload = multer({
-    storage: storage,
-    limits: { fileSize: '1000000' },
-    fileFilter: (req, file, cb) => {
-        const fileTypes = /jpeg|jpg|png|gif/
-        const mimeType = fileTypes.test(file.mimetype)  
-        const extname = fileTypes.test(path.extname(file.originalname))
-  
-        if(mimeType && extname) {
-            return cb(null, true)
-        }
-        cb('Give proper files formate to upload')
-    }
-  }).single('attestation')
+  destination: (req, file, cb) => {
+    cb(null, 'uploadsattestation')
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+})
 
-  // afficher tous les demande
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: '1000000' },
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png|gif/
+    const mimeType = fileTypes.test(file.mimetype)
+    const extname = fileTypes.test(path.extname(file.originalname))
+
+    if (mimeType && extname) {
+      return cb(null, true)
+    }
+    cb('Give proper files formate to upload')
+  }
+}).single('attestation')
+
+// afficher tous les demande
 const FindAllDemande = async (req, res) => {
-    try {
-      const data = await DemandeModel.find().populate('user', ["matricule", "role","nom", "prenom","operation"])
-      res.status(200).json(data)
-  
-    } catch (error) {
-      res.status(404).json(error.message)
-    }
+  try {
+    const data = await DemandeModel.find().populate('user', ["matricule", "role", "nom", "prenom", "operation"])
+    res.status(200).json(data)
+
+  } catch (error) {
+    res.status(404).json(error.message)
   }
-  const modifierDemande = async (req, res) => {
-    const demandeId = req.params.id;
-    const etat = req.body.etat;
-    try {
-      const demande = await DemandeModel.findById(demandeId);
-      if (!demande) {
-        res.status(404).json({ message: "La demande spécifiée n'existe pas" });
-      } else {
-        // Mettre à jour l'état de la demande
-        demande.etat = etat;
-        await demande.save();
-        res.status(200).json({ message: "La demande a été modifiée avec succès", demande });
-      }
-    } catch (error) {
-      res.status(500).json({ message: "Une erreur est survenue lors de la modification de la demande", error });
+}
+const modifierDemande = async (req, res) => {
+  const demandeId = req.params.id;
+  const etat = req.body.etat;
+  try {
+    const demande = await DemandeModel.findById(demandeId);
+    if (!demande) {
+      res.status(404).json({ message: "La demande spécifiée n'existe pas" });
+    } else {
+      // Mettre à jour l'état de la demande
+      demande.etat = etat;
+      await demande.save();
+      res.status(200).json({ message: "La demande a été modifiée avec succès", demande });
     }
-  };
-  
-   // afficher une seul absence par ID
+  } catch (error) {
+    res.status(500).json({ message: "Une erreur est survenue lors de la modification de la demande", error });
+  }
+};
+
+// afficher une seul demande par ID
 const FindDemande = async (req, res) => {
-    try {
-      const demande = await DemandeModel.find({ user: req.user.id }).populate('user', ["matricule", "role","nom", "prenom"])
-      res.status(200).json(demande)
-  
-    } catch (error) {
-      res.status(404).json(error.message)
-    }
+  try {
+    const demande = await DemandeModel.find({ user: req.user.id }).populate('user', ["matricule", "role", "nom", "prenom"])
+    res.status(200).json(demande)
+
+  } catch (error) {
+    res.status(404).json(error.message)
   }
-  const ajouterimage = async (req, res) => {
-    const demandeId = req.params.id;
-    const attestation = req.file;
-    try {
-      const demande = await DemandeModel.findById(demandeId);
-      if (!demande) {
-        res.status(404).json({ message: "La demande spécifiée n'existe pas" });
-      } else {
-        if (attestation) {
-          demande.attestation = attestation.path;
-        }
-        demande.etat = 'traiter'; // modification de l'état de la demande
-        await demande.save();
-        res.status(200).json({ message: "La demande a été modifiée avec succès", demande });
+}
+const ajouterimage = async (req, res) => {
+  const demandeId = req.params.id;
+  const attestation = req.file;
+  try {
+    const demande = await DemandeModel.findById(demandeId);
+    if (!demande) {
+      res.status(404).json({ message: "La demande spécifiée n'existe pas" });
+    } else {
+      if (attestation) {
+        demande.attestation = attestation.path;
       }
-    } catch (error) {
-      res.status(500).json({ message: "Une erreur est survenue lors de la modification de la demande", error });
+      demande.etat = 'Accordé'; // modification de l'état de la demande
+      await demande.save();
+      res.status(200).json({ message: "La demande a été modifiée avec succès", demande });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Une erreur est survenue lors de la modification de la demande", error });
+  }
 };
 
 module.exports = {
-    ajouterimage,
-    FindDemande,
-    modifierDemande,
-    FindAllDemande,
-    upload,
-    ajouterDemande,
+  ajouterimage,
+  FindDemande,
+  modifierDemande,
+  FindAllDemande,
+  upload,
+  ajouterDemande,
 }
--- validators/demande.js
+++ validators/demande.js
@@ -5,14 +5,10 @@ module.exports = function Validatedemande(data) {
   let errors = {};
 
   data.type = !isEmpty(data.type) ? data.type : "";
-  data.commentaire = !isEmpty(data.commentaire) ? data.commentaire : "";
   data.etat = !isEmpty(data.etat) ? data.etat : "";
   
   if (validator.isEmpty(data.type)) {
     errors.type = "Veuillez entrer le type.";
-  }
-  if (validator.isEmpty(data.commentaire)) {
-    errors.commentaire = "Veuillez entrer la commentaire ";
   }
 
   return {
