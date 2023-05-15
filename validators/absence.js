a0c88a9027516ac0c7e6d1eaeb5827cb20e798df
Abir Ferjeni <abiirfr52@gmail.com>
2023-05-15 12:14:29 +0100

    absence ne dois pas passe 15 jpur

--- validators/absence.js
+++ validators/absence.js
@@ -1,27 +1,23 @@
 const isEmpty = require("./isEmpty");
 const validator = require("validator");
-const { isBefore, isAfter } = require("date-fns");
+const { isBefore, isAfter, differenceInCalendarDays } = require("date-fns");
 
 const currentDate = new Date();
 const oneYearFromNow = new Date();
 oneYearFromNow.setFullYear(currentDate.getFullYear() + 1);
 
-
-
-
-module.exports = function ValidateAbsence(data,file) {
+module.exports = function ValidateAbsence(data, file) {
   let errors = {};
 
   data.type = !isEmpty(data.type) ? data.type : "";
   data.dateDebut = !isEmpty(data.dateDebut) ? data.dateDebut : "";
   data.dateFin = !isEmpty(data.dateFin) ? data.dateFin : "";
 
-
   if (validator.isEmpty(data.type)) {
     errors.type = "Veuillez entrer le type d'absence.";
   }
   if (!validator.isDate(data.dateDebut)) {
-    errors.dateDebut = "Veuillez entrer la date de debut de période d'absence.";
+    errors.dateDebut = "Veuillez entrer la date de début de période d'absence.";
   }
   if (!validator.isDate(data.dateFin)) {
     errors.dateFin = "Veuillez entrer la date de fin de période d'absence.";
@@ -47,12 +43,19 @@ module.exports = function ValidateAbsence(data,file) {
     errors.dateFin = "La date de fin d'absence ne doit pas dépasser un an à partir de la date actuelle.";
   }
 
+  // Check the difference between dateDebut and dateFin
   if (
     !validator.isEmpty(data.dateDebut) &&
     !validator.isEmpty(data.dateFin) &&
     isBefore(new Date(data.dateFin), new Date(data.dateDebut))
   ) {
     errors.dateDebut = "La date de début d'absence doit être antérieure à la date de fin d'absence.";
+  } else if (
+    !validator.isEmpty(data.dateDebut) &&
+    !validator.isEmpty(data.dateFin) &&
+    differenceInCalendarDays(new Date(data.dateFin), new Date(data.dateDebut)) > 15
+  ) {
+    errors.dateFin = "La période d'absence ne peut pas dépasser 15 jours.";
   }
 
   return {
